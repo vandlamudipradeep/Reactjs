9cb7bb0f5e20b3ce1c7f44ddc57d63a252697e4d
vadlmudipradeep <vadlamudipradeep200@gmail.com>
2025-05-05 16:58:27 +0530

    initial commit

---
+++ components/Helpers/Wrapper.js
@@ -0,0 +1,11 @@
+const Wrapper = (props)=>{
+
+/* childeren props has can hold all the content
+ that has passing from opening and clonging tags
+ of our coustem component
+
+*/
+return props.children
+};
+
+export default Wrapper;
